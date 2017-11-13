578cca08fcc7370158c2c900a082506896c7a05c
Sergi <elpicart@gmail.com>
2017-11-13 17:58:41 +0100

    Analysis 5.5 (start)

--- 03_config.R
+++ 03_config.R
@@ -13,6 +13,17 @@ dir_metadata <- "00_metadata"
 dir_raw <- "00_rawdata"
 file_alzh <- paste0(dir_raw, "/ben_15092017_ot_ad.txt")
 file_4diseases <- paste0(dir_raw, "/four_disease_associations_ot_public_20171005.txt")
+file_alldiseases <- paste0(dir_raw, "/17.06_ot_commondisease_associations_filtered.csv")
+
+# Thresholds
+# (Ben. Subject: Genetics OT score, 2 nov)
+# The OT genetics threshold score for distinguishing positives 
+# and negatives would be 0.160.
+col_genetic <- "association_score.datatypes.genetic_association"
+threshold_genetic <- .160
+# very important: threshold for diseases on both genetically associated 
+# and drug genes
+threshold_ngenes <- 50
 
 # params network
 params_string <- list(
@@ -32,14 +43,20 @@ dir_performance <- "02_performance"
 graph_alzh <- paste0(dir_data, "/graph_alzh.RData")
 # Section 5.2 gdocs
 dir_data3 <- "03_data"
+dir_data10 <- "10_data"
+
 graph_4disease <- paste0(dir_data3, "/graph_4disease.RData")
 file_descriptive <- paste0(dir_data3, "/descriptive_diseases.csv")
 file_kernel3 <- paste0(dir_kernel, "/Net4_weightsIn01.RData")
+
+graph_alldisease <- paste0(dir_data10, "/graph_alldisease.RData")
+
 dir_performance3 <- "03_performance"
 #dir_scores <- "02_scores"
 # Analysis of the topology
 dir_topology <- "04_topology"
 
+dir_topology11 <- "11_topology"
 #################################
 # Other params
 #################################
